e3a1fc69eff5a2bd68cc2c4e0ce40ec4c10bf367
lee <game9777@naver.com>
2019-12-04 10:36:57 +0900

    Indent change & Remove useless code

--- getppid_test.c
+++ getppid_test.c
@@ -2,20 +2,10 @@
 #include "stat.h"
 #include "user.h"
 
-void
-printf(int fd, const char *s, ...)
-{
-  write(fd, s, strlen(s));
-}
-
 int
 main(void)
 {
-  int pid = fork();
   printf(1, "My pid %d\n", getpid());
-  if (pid == 0) {
-    printf(1, "My pid %d and My ppid %d\n", getpid(), getppid());
-    exit();
-  }
+  printf(1, "My ppid %d\n", getppid());
   return 0;
 }
